e6318951e7e9f02ba4e4329ef15ea16e59f8a7ff
Vladislav <zvlad9923@gmail.com>
2025-01-30 13:22:21 +0200

    version 0.0.5

--- script.ts
+++ script.ts
@@ -1,20 +1,20 @@
 function addImgForLazyLoading (highTarget: HTMLDivElement | null, src: string, highImgTarget: HTMLImageElement | null): void {
-    let imageAdded: boolean = false;
+    let imageAdded: boolean = false; // flag addedImg
 
-    function addPromiseToImg(path: string): Promise<HTMLImageElement> {
+    function addPromiseToImg(path: string): Promise<HTMLImageElement> { // path: src
         return new Promise((resolve, reject) => {
             if (highImgTarget) {
-                highImgTarget.src = path;
+                highImgTarget.src = path; // add src in path
         
                 highImgTarget.addEventListener('load', () => {
                     resolve(highImgTarget);
                 });
         
                 highImgTarget.addEventListener('error', () => {
-                    reject(new Error(`image: ${path} load error`));
+                    reject(new Error(`image: ${path} load error`)); // calling error
                 })
             } else {
-                reject(new Error(`image: ${highImgTarget} not found`))
+                reject(new Error(`image: ${highImgTarget} not found`)) // calling error
             }
         })
     }
@@ -24,11 +24,11 @@ function addImgForLazyLoading (highTarget: HTMLDivElement | null, src: string, h
         if (target && !imageAdded) {
             const rect = target.getBoundingClientRect();
             console.warn('>> scroll tracking <<');
-            if (rect.top >= 0 && rect.left >= 0 && rect.bottom <= window.innerHeight && rect.right <= window.innerWidth) {
+            if (rect.top >= 0 && rect.left >= 0 && rect.bottom <= window.innerHeight && rect.right <= window.innerWidth) { // Show block
                 imageAdded = true;
-                addPromiseToImg(src).then(img => {
-                    document.body.appendChild(img);
-                    document.removeEventListener('scroll', onScroll);
+                addPromiseToImg(src).then(img => { // Calling Promise in addPromiseToImg
+                    document.body.appendChild(img); // add Element
+                    document.removeEventListener('scroll', onScroll); // removeEventListener onScroll
                 }).catch((error) => {
                     console.log(error);
                 })
@@ -38,16 +38,16 @@ function addImgForLazyLoading (highTarget: HTMLDivElement | null, src: string, h
     
     function onScroll() {
         if (highTarget) {
-            addImage(highTarget);
+            addImage(highTarget); // div Element
         }
     }
     
     if (highTarget) {
-        document.addEventListener('scroll', onScroll);
+        document.addEventListener('scroll', onScroll); // addEventListener onScroll
     }
 }
+ 
+const targetLast: HTMLDivElement | null = document.querySelector('#targetLastBlock'); // id div Element
+const lazyLoad__Img: HTMLImageElement | null = document.querySelector('#lazyLoad__Img'); // id img Element
 
-const targetLast: HTMLDivElement | null = document.querySelector('#targetLastBlock');
-const lazyLoad__Img: HTMLImageElement | null = document.querySelector('#lazyLoad__Img');
-
-addImgForLazyLoading(targetLast, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvkVTnPAhOLoRmZwK6-m2VsR-SqRu_meVJ9w&s', lazyLoad__Img);
+addImgForLazyLoading(targetLast, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvkVTnPAhOLoRmZwK6-m2VsR-SqRu_meVJ9w&s', lazyLoad__Img); // Calling of Function
